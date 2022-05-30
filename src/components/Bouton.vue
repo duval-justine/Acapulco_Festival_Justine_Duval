baa2695936287cb3cda6a32a7bc869c54844aa9c
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-30 11:15:14 +0200

    Création du composant Bouton

---
+++ src/components/Bouton.vue
@@ -0,0 +1,18 @@
+<template>
+<RouterLink class="bg-Extended/amber/500 rounded-lg drop-shadow-md" :to="lien">
+    <p class="py-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] drop-shadow-md">{{texte}}</p>
+<RouterLink />
+</template>
+<script>
+import { RouterLink, RouterView } from "vue-router";
+
+export default {
+  props: {
+    texte: String,
+    lien:{
+      type: String,
+      default:"/"
+    },
+  }
+}
+</script>
