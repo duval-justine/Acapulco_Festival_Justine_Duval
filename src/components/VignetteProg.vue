f24edb3410237ab243a57363545fb77dc0e15812
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-30 16:19:42 +0200

    Création du premier carrousel de la page progall

--- src/components/VignetteProg.vue
+++ src/components/VignetteProg.vue
@@ -1,22 +1,34 @@
 <template>
-    <div class=" w-[270px] h-[248px] relative">
-        <img class="absolute opacity-80 w-full object-cover rounded-xl" src="../../public/images/prog_aw.webp" alt="">
+    <div class="mt-[223px] w-[270px] h-[256px] relative">
+        <img class="absolute opacity-80 w-full object-cover rounded-xl" :src="image" alt="">
         <div>
-            <p class="absolute mt-[214px] ml-3 text-[10px] font-lato font-semibold text-left text-Extended/amber/300 ">Jeudi 7 juillet - 23h30 à 3h00</p>
-            <p class="absolute mt-44 ml-3 text-2xl font-concert-one text-Extended/true-gray/50 text-center">Alan Walker</p>
+            <p class="absolute mt-[214px] ml-3 text-[10px] font-lato font-semibold text-left text-Extended/amber/300 ">{{date}}</p>
+            <p class="absolute mt-44 ml-3 text-2xl font-concert-one text-Extended/true-gray/50 text-center">{{nom}}</p>
             <div class="absolute">
-                <Bouton class="absolute mt-[220px] ml-44"
-                texte="Informations"/>
+                <RouterLink :to="lien" >
+                    <button class="mt-[220px] ml-44 bg-Extended/amber/500 rounded-lg drop-shadow-md">
+                        <p class="py-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] drop-shadow-md">Informations</p>
+                    </button>
+                </RouterLink>
             </div>
         </div>
     </div>  
 </template>
 
 <script>
-import Bouton from './Bouton.vue'
+import { RouterLink, RouterView } from "vue-router";
+
 
 export default {
-    components: {Bouton}
+    props: {
+        image: String,
+        date: String,
+        nom: String,
+        lien:{
+            type: String,
+            default:"/"
+        }
+    }
 }
 </script>
 
--- src/pages/ProgrammationAll.vue
+++ src/pages/ProgrammationAll.vue
@@ -3,16 +3,56 @@
         <header >
             <Header/>
         </header>
-        <main>
+        <main class="mt-14">
             <Entete/>
+            <div class="w-full flex gap-5 snap-x overflow-x-auto">
+                <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+                    <Vignette class="ml-4 shrink-0"
+                    image="/images/prog_mg.webp"
+                    nom="Martin Garrix"
+                    date="Samedi 9 juillet - 23h30 à 3h00"/>
+                </div>
+                <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+                    <Vignette class="ml-4 shrink-0"
+                    image="/images/prog_mattn.webp"
+                    nom="MATTN"
+                    date="Samedi 9 & Dimache 10 juillet"/>
+                </div>
+                <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+                    <Vignette class="ml-4 shrink-0"
+                    image="/images/prog_pb.webp"
+                    nom="Petit Biscuit"
+                    date="Vendredi 8 juillet - 20h00 à 23h30"/>
+                </div>
+                <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+                    <Vignette class="ml-4 shrink-0"
+                    image="/images/prog_al.webp"
+                    nom="Amélie Lens"
+                    date="Vendredi 8 & Samedi 9 juillet"/>
+                </div>
+                <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+                    <Vignette class="ml-4 shrink-0"
+                    image="/images/prog_aw.webp"
+                    nom="Alan Walker"
+                    date="Jeudi 7 juillet - 23h30 à 3h00"/>
+                </div>
+                <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+                    <Vignette class="ml-4 shrink-0"
+                    image="/images/prog_lp.webp"
+                    nom="Lilly Palmer"
+                    date="Jeudi 7 & Samedi 9 juillet"/>
+                </div>
+            </div>
+            <p class="mt-72">test</p>
         </main>
     </div>
 </template>
 <script>
 import Header from '../components/Header.vue'
 import Entete from '../components/EnteteProg.vue'
+import Vignette from '../components/VignetteProg.vue'
 
 export default {
-    components: {Header, Entete}
+    components: {Header, Entete, Vignette}
 }
 </script>
