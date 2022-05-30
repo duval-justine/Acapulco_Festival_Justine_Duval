64100a5b14ce696521f00c05fb76b44bd7001c9a
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-30 10:11:45 +0200

    Ajout de l'icone menu dans le header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
-    <Logo class="my-3 ml-8"/>
+    <div class="flex justify-between my-3 ">
+        <Logo class="ml-8"/>
+        <MenuIcon class="mr-8 text-Extended/true-gray/50 w-14"/>
+    </div>
 </template>
 
 <script>
 import Logo from './icons/LogoFestival.vue'
+import {MenuIcon} from '@heroicons/vue/outline'
 
 export default {
-    components: {Logo}
+    components: {Logo, MenuIcon}
 }
 </script>
