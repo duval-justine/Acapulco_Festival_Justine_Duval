956213a027cc05dded3b63750b6665975ddc894e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-30 14:35:39 +0200

    Création du composant de l'entete de la page prog

---
+++ src/components/EnteteProg.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="relative">
+        <img class="absolute w-full object-cover" src="../../public/images/prog_img1.webp" alt="">
+        <h1 class="absolute mt-16 mr-5 font-concert-one text-Extended/true-gray/50 text-3xl">Programmes</h1>
+    </div>
+    <div class="mt-40 flex gap-2 absolute">
+        <Bouton
+        texte="All"
+        lien="/progall"/>
+        <Bouton
+        texte="Jeudi"
+        lien="/"/>
+        <Bouton
+        texte="Vendredi"
+        lien="/"/>
+        <Bouton
+        texte="Samedi"
+        lien="/"/>
+        <Bouton
+        texte="Dimanche"
+        lien="/"/>
+    </div>
+</template>
+
+<script>
+import Bouton from '../components/Bouton.vue'
+
+export default {
+    components: {Bouton}
+}
+</script>
