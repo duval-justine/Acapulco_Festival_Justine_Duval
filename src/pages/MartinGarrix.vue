03f9d0bafa926f54b862939c0b81668f4dfed54e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-30 20:13:43 +0200

    Création de la page de l'artiste Martin Garrix

---
+++ src/pages/MartinGarrix.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <HeaderRetour/>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <Footer/>
+    </footer>
+</template>
+
+<script>
+import HeaderRetour from '../components/HeaderRetour.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+    components: {HeaderRetour, Footer}
+}
+</script>
