c876828aad7a242fae6395aa8985ad48a432308e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-30 17:09:22 +0200

    Création de la page Concert MG

---
+++ src/pages/ConcertMG.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <HeaderRetour/>
+    </header>
+</template>
+
+<script>
+import HeaderRetour from '../components/HeaderRetour.vue'
+
+export default {
+    components: {HeaderRetour}
+}
+</script>
