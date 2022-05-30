d2065c25bed3f8d89ce1642d76f42f7ac0149f27
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-30 11:31:24 +0200

    Résolution de pblm

--- src/components/Bouton.vue
+++ src/components/Bouton.vue
@@ -1,7 +1,7 @@
 <template>
-<RouterLink class="bg-Extended/amber/500 rounded-lg drop-shadow-md" :to="lien">
+<RouterLink :to="lien" class="bg-Extended/amber/500 rounded-lg drop-shadow-md" >
     <p class="py-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] drop-shadow-md">{{texte}}</p>
-<RouterLink />
+</RouterLink>
 </template>
 <script>
 import { RouterLink, RouterView } from "vue-router";
---
+++ src/components/CarrouselAcc.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-Extended/true-gray/900">
+        <img class="w-full object-cover" :src="image" alt="">
+        <p class="mx-32 py-2 font-lato text-Extended/true-gray/50 text-sm drop-shadow-md">{{texte}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String, 
+    texte: String,
+  }
+}
+</script>
