a7cd79b9266f6804a5dbe51c189a21258fc5b380
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-31 20:13:43 +0200

    Fin du responsive de la page all prog

--- src/pages/ProgrammationAll.vue
+++ src/pages/ProgrammationAll.vue
@@ -121,7 +121,9 @@
       <!-- Format Desktop -->
       <div class="hidden flex-col md:flex">
         <div class="mx-12 mt-20 mb-20 grid w-11/12 grid-cols-[repeat(auto-fit,minmax(300px,1fr))] items-center justify-between gap-7">
-          <Vignette image="/images/prog_dmg.webp" nom="Martin Garrix" date="Samedi 9 juillet - 23h30 à 3h00" />
+          <RouterLink to="/concertMG">
+            <Vignette image="/images/prog_dmg.webp" nom="Martin Garrix" date="Samedi 9 juillet - 23h30 à 3h00" />
+          </RouterLink>
           <Vignette image="/images/prog_dmattn.webp" nom="MATTN" date="Samedi 9 & Dimanche 10 juillet" />
           <Vignette image="/images/prog_dawpb.webp" nom="Petit Biscuit" date="Vendredi 8 juillet - 20h00 à 23h30" />
           <Vignette image="/images/prog_dal.webp" nom="Amélie Lens" date="Vendredi 8 & Samedi 9 juillet" />
@@ -141,12 +143,28 @@
           <Vignette image="/images/prog_dhfr.webp" nom="Henri PFR" date="Samedi 9 & Dimanche 10 juillet" />
           <Vignette image="/images/prog_dawjc.webp" nom="Joel Corry" date="Jeudi 7 juillet - 18h00 à 20h00" />
           <Vignette image="/images/prog_dawlp.webp" nom="Lilly Palmer" date="Jeudi 7 & Samedi 9 juillet" />
-          <Vignette image="/images/prog_dmg.webp" nom="Martin Garrix" date="Samedi 9 juillet - 23h30 à 3h00" />
+          <RouterLink to="/concertMG">
+            <Vignette image="/images/prog_dmg.webp" nom="Martin Garrix" date="Samedi 9 juillet - 23h30 à 3h00" />
+          </RouterLink>
           <Vignette image="/images/prog_dmattn.webp" nom="MATTN" date="Samedi 9 & Dimanche 10 juillet" />
           <Vignette image="/images/prog_dofen.webp" nom="Ofenbach" date="Samedi 9 et Dimanche 10 juillet" />
           <Vignette image="/images/prog_dawpb.webp" nom="Petit Biscuit" date="Vendredi 8 juillet - 20h00 à 23h30" />
           <Vignette image="/images/prog_dpp.webp" nom="Pretty Pink" date="Jeudi 7 juillet - 20h00 à 23h30" />
           <Vignette image="/images/prog_dtopic.webp" nom="Topic" date="Vendredi 8 juillet - 23h30 à 3h00" />
+        </div>
+        <div class="flex gap-2 pt-8">
+          <h4 class="ml-6 font-lato text-xl text-Extended/true-gray/50">Nos jeunes artistes</h4>
+          <Line class="mt-2" />
+        </div>
+        <div class="mx-12 mt-20 mb-20 grid w-11/12 grid-cols-[repeat(auto-fit,minmax(300px,1fr))] items-center justify-between gap-7">
+          <Vignette image="/images/prog_dlf.webp" nom="Lost Frequencies" date="Vendredi 8 juillet - 20h00 à 23h30" />
+          <Vignette image="/images/prog_dlpg.webp" nom="LP Giobbi" date="Vendredi 8 juillet - 23h30 à 3h00" />
+          <Vignette image="/images/prog_dmadeon.webp" nom="Madeon" date="Samedi 9 juillet - 23h30 à 3h00" />
+          <Vignette image="/images/prog_deluize.webp" nom="Eluize" date="Samedi 9 juillet - 20h00 à 23h30" />
+          <Vignette image="/images/prog_dmt.webp" nom="Marina Trench" date="Jeudi 7 juillet - 20h00 à 23h30" />
+          <Vignette image="/images/prog_dmw.webp" nom="Mike Williams" date="Jeudi 7 juillet - 23h30 à 3h00" />
+          <Vignette image="/images/prog_dsh.webp" nom="Seth Hills" date="Vendredi 8 juillet - 16h00 à 18h00" />
+          <Vignette image="/images/prog_durt.webp" nom="U.R.TRAX" date="Vendredi 8 juillet - 18h00 à 20h00" />
         </div>
       </div>
     </main>
