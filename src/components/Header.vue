341380e94a959d500f80a737caa79f17388e2f0a
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-31 11:38:13 +0200

    Modif de la mise en page du menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,14 +10,16 @@
             <span class="sr-only">Menu</span>
         </button>
     </div>
-    <div id="menu" class="z-40 fixed inset-0 translate-x-full bg-Extended/true-gray/900 motion-safe:duration-1000 motion-safe:transition-transform" v-if="menuOuvert"
+    <div id="menu" class="z-40 fixed inset-0 translate-x-full bg-Extended/true-gray/900 motion-safe:duration-1000 motion-safe:transition-transform h-full" v-if="menuOuvert"
       :class="{ 'translate-x-0': menuOuvert }">
         <div class="flex flex-row justify-between relative z-50">
           <button class="mt-52 text-Extended/true-gray/50" aria-haspopup="true" aria-controls="menu" :aria-expanded="menuOuvert" @click="menuOuvert = !menuOuvert">
             <XIcon :class="{'motion-safe:animate-[BackInRight_1s_cubic-bezier(0,1,.24,1)]' : menuOuvert}"/>
           </button>
         </div>
-
+        <div>
+          <DecoBD class="-mt-52"/>
+        </div>
         <nav>
           <ul class="flex flex-col text-center gap-10 font-concert-one text-Extended/true-gray/50 text-3xl">
            <RouterLink :class="{' motion-safe:animate-[Depart_0.05s_cubic-bezier(.5,.5,.5,.5)]' : menuOuvert}" to="/">Accueil</RouterLink>
@@ -27,15 +29,20 @@
            <RouterLink :class="{' motion-safe:animate-[Depart_0.4s_cubic-bezier(.5,.5,.5,.5)]' : menuOuvert}" to="/">Contact</RouterLink>
           </ul>
         </nav>
+        <div >
+          <DecoHG class=""/>
+        </div>
       </div>
 </template>
 
 <script>
 import Logo from './icons/LogoFestival.vue'
+import DecoHG from './icons/DecoHG.vue'
+import DecoBD from './icons/DecoBD.vue'
 import {MenuIcon, XIcon} from '@heroicons/vue/outline'
 
 export default {
-    components: {Logo, MenuIcon, XIcon},
+    components: {Logo, MenuIcon, XIcon, DecoHG, DecoBD},
 
     data() {
         return {
