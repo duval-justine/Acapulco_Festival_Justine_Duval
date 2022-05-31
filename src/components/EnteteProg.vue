8b02660d0150e6cb39ba339905390458422da346
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-31 10:06:41 +0200

    Modif bouton prog all

--- src/components/EnteteProg.vue
+++ src/components/EnteteProg.vue
@@ -4,21 +4,21 @@
         <h1 class="absolute mt-16 ml-5  font-concert-one text-Extended/true-gray/50 text-3xl">Programmes</h1>
     </div>
     <div class="mt-40 ml-5 flex gap-2 absolute">
-        <Bouton
-        texte="All"
-        lien="/progall"/>
-        <Bouton
-        texte="Jeudi"
-        lien="/"/>
-        <Bouton
-        texte="Vendredi"
-        lien="/"/>
-        <Bouton
-        texte="Samedi"
-        lien="/"/>
-        <Bouton
-        texte="Dimanche"
-        lien="/"/>
+        <RouterLink to="/progall" class="bg-Extended/amber/500 rounded-md drop-shadow-mdpy-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] ">
+        All
+        </RouterLink>
+        <RouterLink to="/progall" class="bg-Extended/amber/500 rounded-md drop-shadow-mdpy-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] ">
+        Jeudi
+        </RouterLink>
+        <RouterLink to="/progall" class="bg-Extended/amber/500 rounded-md drop-shadow-mdpy-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] ">
+        Vendredi
+        </RouterLink>
+        <RouterLink to="/progall" class="bg-Extended/amber/500 rounded-md drop-shadow-mdpy-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] ">
+        Samedi
+        </RouterLink>
+        <RouterLink to="/progall" class="bg-Extended/amber/500 rounded-md drop-shadow-mdpy-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] ">
+        Dimanche
+        </RouterLink>
     </div>
 </template>
 
@@ -26,6 +26,9 @@
 import Bouton from '../components/Bouton.vue'
 
 export default {
-    components: {Bouton}
+    components: {Bouton}, 
+    props: {
+        
+    }
 }
 </script>
