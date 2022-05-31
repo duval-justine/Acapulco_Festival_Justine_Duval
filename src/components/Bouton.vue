683c9874481de99a916297442c5e633096eaa9ca
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-31 17:32:23 +0200

    Bouton en boolean

--- src/components/Bouton.vue
+++ src/components/Bouton.vue
@@ -1,6 +1,6 @@
 <template>
 <RouterLink :to="lien" >
-  <button class="bg-Extended/amber/500 rounded-lg drop-shadow-md">
+  <button class=" bg-Extended/true-gray/900 border-2 border-Extended/amber/500 rounded-lg drop-shadow-md" :class="{'bg-Extended/amber/500':actif}">
     <p class="py-2 px-3 font-lato text-Extended/true-gray/50 text-[12px] drop-shadow-md md:text-base lg:text-lg">{{texte}}</p>
   </button>
 </RouterLink>
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from "vue-router";
 export default {
   props: {
     texte: String,
+    actif: Boolean,
     lien:{
       type: String,
       default:"/"
