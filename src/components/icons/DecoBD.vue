88cad15070599b38d1ac6e9fb5a025a22638cd3d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-31 10:09:18 +0200

    importation des deco du menu

---
+++ src/components/icons/DecoBD.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+  width="184"
+  height="213"
+  viewBox="0 0 184 213"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[183.5px] h-[212.5px]"
+  preserveAspectRatio="none"
+>
+  <path
+    d="M97.5 100.5C155.9 74.9 179.167 22.8333 183.5 0H0.5V212.5C8.5 185.833 39.1 126.1 97.5 100.5Z"
+    fill="#FCD34D"
+  ></path>
+  <path
+    d="M92.5 89C145 62 162.167 22.8333 166.5 0H0L0.5 172.5C8.5 145.833 35.795 118.163 92.5 89Z"
+    fill="#F59E0B"
+  ></path>
+</svg>
+</template>
---
+++ src/components/icons/DecoHG.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+  width="184"
+  height="213"
+  viewBox="0 0 184 213"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[183.5px] h-[212.5px]"
+  preserveAspectRatio="none"
+>
+  <path
+    d="M86 112C27.6 137.6 4.33333 189.667 0 212.5H183V0C175 26.6667 144.4 86.4 86 112Z"
+    fill="#FCD34D"
+  ></path>
+  <path
+    d="M91 123.5C38.5 150.5 21.3333 189.667 17 212.5H183.5L183 40C175 66.6667 147.705 94.3374 91 123.5Z"
+    fill="#F59E0B"
+  ></path>
+</svg>
+</template>
