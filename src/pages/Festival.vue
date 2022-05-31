3f11b6c5695ee2c978f75dd50c4b44e3bf3a5c7f
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-31 09:48:12 +0200

    Ajout des images et du contenu de la page festival

--- src/pages/Festival.vue
+++ src/pages/Festival.vue
@@ -3,7 +3,9 @@
         <Header class="z-10 relative"/>
     </header>
     <main>
-    <img class="-mt-[85px] max-h-[812px] min-h-[805px] opacity-80 w-full object-cover" src="../../public/images/festi_fond.webp" alt="">
+    <img class="-mt-[85px] relative max-h-[812px] min-h-[805px] opacity-80 w-full object-cover" src="../../public/images/festi_fond.webp" alt="">
+    <h1 class="absolute ml-8 -mt-[600px] text-5xl font-concert-one text-Extended/true-gray/50">Acapul'co</h1>
+    <h2 class="absolute ml-[214px] -mt-[500px] text-3xl font-concert-one text-Extended/true-gray/50">Festival</h2>
     <div class="mt-7 mx-7 font-lato text-xs text-Extended/true-gray/50">
         <p>Acapul’co est un Festival situé dans le département de l’Aude est plus précisement dans le petit village de Gruissan.</p>
         <p class="mt-4"> C’est un festival de musique principalement de l’éléctro avec des dj qui viennent du monde entier spécialement pour vous ! </p>
@@ -26,11 +28,59 @@
    </div>
    <h2 class="mt-10 ml-7 font-lato text-sm text-Extended/true-gray/50 font-bold">Deux grandes scènes</h2>
    <div class="mt-3 flex justify-center">
-    <img class="rounded-[40px] w-[312px] h-[189px] object-cover" src="../../public/images/festi_pulcoplaya.webp" alt="">
+    <img class="relative rounded-[40px] w-[312px] h-[189px] opacity-80 object-cover" src="../../public/images/festi_pulcoplaya.webp" alt="">
+    <p class= "absolute my-16 text-4xl font-concert-one text-Extended/true-gray/50">Pulco Playa</p>
    </div>
    <div class="mt-3 flex justify-center">
-    <img class="rounded-[40px] w-[312px] h-[189px] object-cover" src="../../public/images/festi_acaplaya.webp" alt="">
+    <img class="relative rounded-[40px] w-[312px] h-[189px] opacity-80 object-cover" src="../../public/images/festi_acaplaya.webp" alt="">
+     <p class= "absolute my-16 text-4xl font-concert-one text-Extended/true-gray/50">Aca Playa</p>
    </div>
+   <h2 class="mt-10 ml-7 font-lato text-sm text-Extended/true-gray/50 font-bold">Retour en images</h2>
+   <h4 class="mt-5 ml-7 font-concert-one text-sm text-Extended/amber/300 font-bold">Summer</h4>
+    <div class=" mt-2 w-full flex gap-0 snap-x overflow-x-auto">
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_sum1.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_sum2.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_sum3.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_sum4.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_sum5.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_sum6.webp" alt="">  
+        </div>
+    </div>
+    <h4 class="mt-5 ml-7 font-concert-one text-sm text-Extended/amber/300 font-bold">Night</h4>
+    <div class=" mt-2 w-full flex gap-0 snap-x overflow-x-auto">
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_night1.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_night2.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_night3.webp" alt="">  
+        </div>
+    </div>
+    <h4 class="mt-5 ml-7 font-concert-one text-sm text-Extended/amber/300 font-bold">Fireworks</h4>
+    <div class=" mt-2 pb-5 w-full flex gap-0 snap-x overflow-x-auto">
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_fire1.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_fire2.webp" alt="">  
+        </div>
+        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
+              <img class="w-full object-cover" src="../../public/images/festi_fire3.webp" alt="">  
+        </div>
+    </div>
     </main>
     <footer>
         <Footer/>
