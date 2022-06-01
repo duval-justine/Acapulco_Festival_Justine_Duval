6a18be3866a25c91b7e864476fb5f30c747f2e70
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-06-01 14:31:28 +0200

    Début de la mise en page artiste

--- src/components/VignetteArtiste.vue
+++ src/components/VignetteArtiste.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="relative mx-11 flex justify-center">
-        <div class="absolute mt-[90px] w-[287px] h-[190px] rounded-16xl bg-Extended/amber/300" ></div>
-        <div class="absolute ml-8 mt-[105px] flex flex-col gap-4">
-            <a href="https://www.instagram.com/martingarrix/?hl=fr"><Insta class="w-5 h-5"/></a>
-            <a href="https://twitter.com/MartinGarrix"><Twitter class="w-5 h-5"/></a>
-            <a href="https://www.facebook.com/martin.garrix"><Facebook class="w-5 h-5"/></a>
-            <a href="https://www.youtube.com/channel/UC5H_KXkPbEsGs0tFt8R35mA"><YouTube class="w-5 h-5"/></a>
-            <a href="https://martingarrix.com/"><LinkIcon class="w-5 h-5 text-Extended/true-gray/50"/></a>
-        </div>
-        <img class="absolute ml-4 mt-10" src="../../public/images/martin_garrix.png" alt="">
+  <div class="relative mx-11 flex justify-center">
+    <div class="absolute mt-[90px] h-[190px] w-[287px] rounded-16xl bg-Extended/amber/300"></div>
+    <div class="absolute ml-8 mt-[105px] flex flex-col gap-4">
+      <a href="https://www.instagram.com/martingarrix/?hl=fr"><Insta class="h-5 w-5" /></a>
+      <a href="https://twitter.com/MartinGarrix"><Twitter class="h-5 w-5" /></a>
+      <a href="https://www.facebook.com/martin.garrix"><Facebook class="h-5 w-5" /></a>
+      <a href="https://www.youtube.com/channel/UC5H_KXkPbEsGs0tFt8R35mA"><YouTube class="h-5 w-5" /></a>
+      <a href="https://martingarrix.com/"><LinkIcon class="h-5 w-5 text-Extended/true-gray/50" /></a>
     </div>
+    <img class="absolute ml-4 mt-10" src="../../public/images/martin_garrix.png" alt="" />
+  </div>
 </template>
 
 <script>
-import Insta from '../components/icons/Insta.vue'
-import Twitter from '../components/icons/Twitter.vue'
-import Facebook from '../components/icons/Facebook.vue'
-import YouTube from '../components/icons/YouTube.vue'
-import {LinkIcon} from '@heroicons/vue/outline'
+import Insta from "../components/icons/Insta.vue";
+import Twitter from "../components/icons/Twitter.vue";
+import Facebook from "../components/icons/Facebook.vue";
+import YouTube from "../components/icons/YouTube.vue";
+import { LinkIcon } from "@heroicons/vue/outline";
 
 export default {
-    components: {Insta, Twitter, Facebook, YouTube, LinkIcon}
-}
+  components: { Insta, Twitter, Facebook, YouTube, LinkIcon },
+};
 </script>
