e82e28c760df40c6b7d2c79389a80abc1f0f3c9c
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-06-01 08:38:09 +0200

    Page Concert Responsive

--- src/components/VignetteConcert.vue
+++ src/components/VignetteConcert.vue
@@ -1,33 +1,35 @@
 <template>
-<div class="w-full h-[362px] relative -mt-[85px]">
-    <img class="absolute max-h-[570px] min-h-[362px] w-full object-cover opacity-40" :src="image" alt="">
-    <div class="flex flex-col justify-start items-start absolute ml-8 mt-64 gap-3">
-        <p class="self-stretch flex-grow-0 flex-shrink-0 text-2xl font-bold font-lato text-left text-Extended/true-gray/50">{{nom}}</p>
-        <div class="flex justify-start items-start self-stretch flex-grow-0 flex-shrink-0 gap-9">
-            <div class="flex justify-start items-start flex-grow relative gap-2.5">
-                <Calendrier/>
-                <p class="font-lato flex-grow-0 flex-shrink-0 text-xs text-left text-Extended/true-gray/50">{{date}}</p>
-            </div>
-            <div class="flex justify-start items-start flex-grow relative gap-2.5">
-                <ClockIcon class="flex-grow-0 flex-shrink-0 w-5 h-5 relative text-Extended/amber/300"/>
-                <p class="font-lato flex-grow-0 flex-shrink-0 text-xs text-left text-Extended/true-gray/50">{{heure}}</p>
-            </div>
+  <div class="relative -mt-[85px] h-[362px] w-full">
+    <img class="absolute max-h-[570px] min-h-[362px] w-full object-cover opacity-40" :src="image" alt="" />
+    <div class="absolute ml-8 mt-64 flex flex-col items-start justify-start gap-3 lg:mt-64">
+      <p class="flex-shrink-0 flex-grow-0 self-stretch text-left font-lato text-2xl font-bold text-Extended/true-gray/50 lg:text-[65px]">
+        {{ nom }}
+      </p>
+      <div class="flex flex-shrink-0 flex-grow-0 items-start justify-start gap-9 self-stretch">
+        <div class="relative flex flex-grow items-start justify-start gap-2.5">
+          <Calendrier class="lg:h-10 lg:w-10" />
+          <p class="flex-shrink-0 flex-grow-0 text-left font-lato text-xs text-Extended/true-gray/50 lg:text-2xl">{{ date }}</p>
         </div>
+        <div class="relative flex flex-grow items-start justify-start gap-2.5">
+          <ClockIcon class="relative h-5 w-5 flex-shrink-0 flex-grow-0 text-Extended/amber/300 lg:h-10 lg:w-10" />
+          <p class="flex-shrink-0 flex-grow-0 text-left font-lato text-xs text-Extended/true-gray/50 lg:text-2xl">{{ heure }}</p>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import Calendrier from '../components/icons/Calendrier.vue'
-import {ClockIcon} from '@heroicons/vue/outline'
+import Calendrier from "../components/icons/Calendrier.vue";
+import { ClockIcon } from "@heroicons/vue/outline";
 
 export default {
-    components: {Calendrier, ClockIcon},
-    props: {
-        image: String,
-        nom: String,
-        date: String,
-        heure: String, 
-    }
-}
+  components: { Calendrier, ClockIcon },
+  props: {
+    image: String,
+    nom: String,
+    date: String,
+    heure: String,
+  },
+};
 </script>
--- src/pages/ConcertMG.vue
+++ src/pages/ConcertMG.vue
@@ -1,44 +1,81 @@
 <template>
-    <header>
-        <HeaderRetour/>
-    </header>
-    <main class="bg-Extended/true-gray/900">
-        <Vignette
-        image="/images/concert_mg.webp"
-        nom="Martin Garrix"
-        date="Samedi 9 juillet 2022"
-        heure="23h30 - 3h00"/>
-        <div class="mx-8">
-            <h3 class="font-lato text-sm text-Extended/true-gray/50">Description :</h3>
-            <p class="mt-2 font-lato text-xs text-Extended/true-gray/50">Martin Garrix est un DJ hollandais né en 1996 à Amsterdam. A l’âge de seulement 16 ans il sort son premier titre “Keygen” qui est un morceau électro dans un style épuré. En 2013, il sort son single “Animals” qui devient célèbre dans la scène de l’électro. </p>
-            <RouterLink to="/martingarrix" class="mt-2 relative font-lato text-xs text-Extended/amber/500 flex justify-end">
-             Lire la suite
-            </RouterLink>
-        </div>
-        <div class="mx-8">
-            <h3 class="font-lato text-sm text-Extended/true-gray/50">Lieu :</h3>
-            <p class="mt-2 font-lato text-xs text-Extended/true-gray/50">Scène Pul’co Playa - Plage des Ayguades</p>
-            <div class="mt-2 flex justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-[13px] lg:hidden">
-                <img class="flex-grow-0 flex-shrink-0 object-cover w-[147px] h-[138px] rounded-[10px]" src="../../public/images/concert_scene1.webp" alt="">
-                <img class="flex-grow-0 flex-shrink-0 object-cover w-[147px] h-[138px] rounded-[10px]" src="../../public/images/concert_scene1_carte.webp" alt="">
-            </div>
-            <div class="mt-2 justify-center items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-16 hidden lg:flex">
-                <img class="flex-grow-0 flex-shrink-0 object-cover w-[330px] h-[310px] rounded-[10px]" src="../../public/images/concert_scene1.webp" alt="">
-                <img class="flex-grow-0 flex-shrink-0 object-cover w-[330px] h-[310px] rounded-[10px]" src="../../public/images/concert_scene1_carte.webp" alt="">
-            </div>
-        </div>
-    </main>
-    <footer>
-        <Footer/>
-    </footer>
+  <!-- Header -->
+  <header>
+    <HeaderRetour />
+  </header>
+  <!-- Contenus -->
+  <main class="bg-Extended/true-gray/900">
+    <!-- En tête -->
+    <Vignette image="/images/concert_mg.webp" nom="Martin Garrix" date="Samedi 9 juillet 2022" heure="23h30 - 3h00" />
+    <!-- Description -->
+    <div class="mx-8 md:mx-11 lg:mx-44 lg:mt-60">
+      <h3 class="font-lato text-sm text-Extended/true-gray/50 md:text-xl lg:text-2xl">Description :</h3>
+      <p class="mt-2 font-lato text-xs text-Extended/true-gray/50 md:text-base lg:text-xl">
+        Martin Garrix est un DJ hollandais né en 1996 à Amsterdam. A l’âge de seulement 16 ans il sort son premier titre “Keygen” qui est un
+        morceau électro dans un style épuré. En 2013, il sort son single “Animals” qui devient célèbre dans la scène de l’électro.
+      </p>
+      <RouterLink
+        to="/martingarrix"
+        class="relative mt-2 flex justify-end font-lato text-xs text-Extended/amber/500 md:text-base lg:text-xl"
+      >
+        Lire la suite
+      </RouterLink>
+    </div>
+    <!-- Lieu -->
+    <div class="mx-8 md:mx-11 lg:mx-44">
+      <h3 class="font-lato text-sm text-Extended/true-gray/50 md:text-xl lg:text-2xl">Lieu :</h3>
+      <p class="mt-2 font-lato text-xs text-Extended/true-gray/50 md:text-base lg:text-xl">Scène Pul’co Playa - Plage des Ayguades</p>
+      <div class="relative mt-2 flex flex-shrink-0 flex-grow-0 items-start justify-start gap-[13px] self-stretch md:hidden">
+        <img
+          class="h-[138px] w-[147px] flex-shrink-0 flex-grow-0 rounded-[10px] object-cover"
+          src="../../public/images/concert_scene1.webp"
+          alt=""
+        />
+        <img
+          class="h-[138px] w-[147px] flex-shrink-0 flex-grow-0 rounded-[10px] object-cover"
+          src="../../public/images/concert_scene1_carte.webp"
+          alt=""
+        />
+      </div>
+      <!-- Ipad -->
+      <div class="relative mt-2 hidden flex-shrink-0 flex-grow-0 items-start justify-center gap-16 self-stretch pb-5 md:flex">
+        <img
+          class="h-[254px] w-[248px] flex-shrink-0 flex-grow-0 rounded-[10px] object-cover"
+          src="../../public/images/concert_scene1.webp"
+          alt=""
+        />
+        <img
+          class="h-[254px] w-[248px] flex-shrink-0 flex-grow-0 rounded-[10px] object-cover"
+          src="../../public/images/concert_scene1_carte.webp"
+          alt=""
+        />
+      </div>
+      <!-- Desktop -->
+      <div class="relative mt-2 hidden flex-shrink-0 flex-grow-0 items-start justify-center gap-16 self-stretch pb-5 lg:flex">
+        <img
+          class="h-[310px] w-[330px] flex-shrink-0 flex-grow-0 rounded-[10px] object-cover"
+          src="../../public/images/concert_scene1.webp"
+          alt=""
+        />
+        <img
+          class="h-[310px] w-[330px] flex-shrink-0 flex-grow-0 rounded-[10px] object-cover"
+          src="../../public/images/concert_scene1_carte.webp"
+          alt=""
+        />
+      </div>
+    </div>
+  </main>
+  <footer>
+    <Footer />
+  </footer>
 </template>
 
 <script>
-import HeaderRetour from '../components/HeaderRetour.vue'
-import Vignette from '../components/VignetteConcert.vue'
-import Footer from '../components/Footer.vue'
+import HeaderRetour from "../components/HeaderRetour.vue";
+import Vignette from "../components/VignetteConcert.vue";
+import Footer from "../components/Footer.vue";
 
 export default {
-    components: {HeaderRetour, Vignette, Footer}
-}
+  components: { HeaderRetour, Vignette, Footer },
+};
 </script>
