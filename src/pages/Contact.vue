f347671f5fd0b684b21e667f9e08d9e3316ba8b8
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-06-01 12:01:26 +0200

    Résolution du prblm de footer page contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,9 +4,9 @@
   </header>
   <main>
     <!-- Google Map -->
-    <div class="relative">
+    <div>
       <iframe
-        class="absolute -mt-10 h-[316px] w-full object-cover opacity-60 md:-mt-20 lg:h-[500px]"
+        class="-mt-10 h-[316px] w-full object-cover opacity-60 md:-mt-20 lg:h-[500px]"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11648.772046710292!2d3.1246769358562494!3d43.12147141628794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b0ffcdfcdf06c1%3A0xa96356bc0de41a8a!2sPlage%20de%20Mateille!5e0!3m2!1sfr!2sfr!4v1654071530761!5m2!1sfr!2sfr"
         width="600"
         height="450"
@@ -16,12 +16,14 @@
         referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
       <div class="flex justify-center">
-        <h1 class="md: absolute mt-40 font-concert-one text-3xl text-Extended/true-gray/50 md:mt-20 md:text-7xl lg:text-8xl">
+        <h1 class="relative z-20 -mt-40 font-concert-one text-3xl text-Extended/true-gray/50 md:-mt-20 md:text-7xl lg:text-8xl">
           Nous contacter
         </h1>
       </div>
-      <div class="flex justify-center">
-        <div class="absolute mt-56 w-[267px] bg-Extended/true-gray/900 py-3 md:h-[1273.01px] md:w-[729.09px] lg:h-[1055px] lg:w-[1170px]">
+      <div class="flex justify-center pb-12">
+        <div
+          class="relative z-20 -mt-10 w-[267px] bg-Extended/true-gray/900 py-3 md:h-[1273.01px] md:w-[729.09px] lg:h-[1055px] lg:w-[1170px]"
+        >
           <form class="mx-6 font-lato text-Extended/true-gray/50" id="form">
             <label for="fname" class="mx-6 text-xs md:text-2xl">Nom </label>
             <br />
