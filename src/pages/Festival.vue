5f8f25226c9dde2e64e7c1aaa2ef41fcae62dece
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-06-01 12:35:55 +0200

    Début responsive age festival

--- src/pages/Festival.vue
+++ src/pages/Festival.vue
@@ -1,97 +1,195 @@
 <template>
-    <header>
-        <Header class="z-10 relative"/>
-    </header>
-    <main>
-    <img class="-mt-[85px] relative max-h-[812px] min-h-[805px] opacity-80 w-full object-cover" src="../../public/images/festi_fond.webp" alt="">
-    <h1 class="absolute ml-8 -mt-[600px] text-5xl font-concert-one text-Extended/true-gray/50">Acapul'co</h1>
-    <h2 class="absolute ml-[214px] -mt-[500px] text-3xl font-concert-one text-Extended/true-gray/50">Festival</h2>
-    <div class="mt-7 mx-7 font-lato text-xs text-Extended/true-gray/50">
+  <header>
+    <Header class="relative z-10" />
+  </header>
+  <main>
+    <img
+      class="relative -mt-[85px] max-h-[812px] min-h-[805px] w-full object-cover opacity-80"
+      src="../../public/images/festi_fond.webp"
+      alt=""
+    />
+    <h1 class="absolute ml-8 -mt-[600px] font-concert-one text-5xl text-Extended/true-gray/50">Acapul'co</h1>
+    <h2 class="absolute ml-[214px] -mt-[500px] font-concert-one text-3xl text-Extended/true-gray/50">Festival</h2>
+    <!-- Format Mobile -->
+    <div class="md:hidden">
+      <div class="mx-7 mt-7 font-lato text-xs text-Extended/true-gray/50">
         <p>Acapul’co est un Festival situé dans le département de l’Aude est plus précisement dans le petit village de Gruissan.</p>
-        <p class="mt-4"> C’est un festival de musique principalement de l’éléctro avec des dj qui viennent du monde entier spécialement pour vous ! </p>
-        <p class="mt-4">Le festival est composé de deux grandes scènes, de plusieurs espaces pour pouvoir s’hydrater et se ressoucer. Ainsi que des espaces pour vous amusez et remportez des goodies. Et à la fin de la soirée nous vous préparons d’autres surprises qui sont à venir découvrir...</p>
+        <p class="mt-4">
+          C’est un festival de musique principalement de l’éléctro avec des dj qui viennent du monde entier spécialement pour vous !
+        </p>
+        <p class="mt-4">
+          Le festival est composé de deux grandes scènes, de plusieurs espaces pour pouvoir s’hydrater et se ressoucer. Ainsi que des
+          espaces pour vous amusez et remportez des goodies. Et à la fin de la soirée nous vous préparons d’autres surprises qui sont à
+          venir découvrir...
+        </p>
+      </div>
+      <div class="mt-3 flex justify-center">
+        <img
+          class="h-[189px] w-[312px] rounded-[40px] object-cover opacity-80"
+          src="../../public/images/festi_img1.webp"
+          alt="Personnes qi font la fête"
+        />
+      </div>
+      <h2 class="mt-10 ml-7 font-lato text-sm font-bold text-Extended/true-gray/50">La plage de Mateille</h2>
+      <div class="mx-7 mt-3 flex justify-center">
+        <img src="../../public/images/festi_carte.png" alt="" />
+      </div>
+      <div class="mx-7 mt-6 font-lato text-xs text-Extended/true-gray/50">
+        <p>
+          La plage de Mateille est situé entre l’Étang de Mateille et l’entrée du port. C’est l’une des plages les plus sauvages de Gruissan
+          et elle s’étend sur 1,2km.
+        </p>
+        <p class="mt-4">
+          Pour vous rendre sur cette plage depuis le port de Gruissan, il suffit de prendre l’avenue des plages, puis de passer le pont qui
+          passe au dessus de l’étang de Mateille.
+        </p>
+        <p class="mt-4">
+          Cette plage dispose d’un grand parking situé juste après le petit pont qui passe au dessus de l’Étang de Mateille.
+        </p>
+      </div>
+      <div class="mt-3 flex justify-center">
+        <img class="h-[189px] w-[312px] rounded-[40px] object-cover" src="../../public/images/festi_img2.webp" alt="" />
+      </div>
+      <h2 class="mt-10 ml-7 font-lato text-sm font-bold text-Extended/true-gray/50">Deux grandes scènes</h2>
+      <div class="mt-3 flex justify-center">
+        <img
+          class="relative h-[189px] w-[312px] rounded-[40px] object-cover opacity-80"
+          src="../../public/images/festi_pulcoplaya.webp"
+          alt=""
+        />
+        <p class="absolute my-16 font-concert-one text-4xl text-Extended/true-gray/50">Pulco Playa</p>
+      </div>
+      <div class="mt-3 flex justify-center">
+        <img
+          class="relative h-[189px] w-[312px] rounded-[40px] object-cover opacity-80"
+          src="../../public/images/festi_acaplaya.webp"
+          alt=""
+        />
+        <p class="absolute my-16 font-concert-one text-4xl text-Extended/true-gray/50">Aca Playa</p>
+      </div>
     </div>
-    <div class="mt-3 flex justify-center">
-    <img class="rounded-[40px] w-[312px] h-[189px] opacity-80 object-cover" src="../../public/images/festi_img1.webp" alt="">
-   </div>
-   <h2 class="mt-10 ml-7 font-lato text-sm text-Extended/true-gray/50 font-bold">La plage de Mateille</h2>
-   <div class="mt-3 mx-7 flex justify-center">
-       <img src="../../public/images/festi_carte.png" alt="">
-   </div>
-   <div class="mt-6 mx-7 font-lato text-xs text-Extended/true-gray/50">
-       <p>La plage de Mateille est situé entre l’Étang de Mateille et l’entrée du port. C’est l’une des plages les plus sauvages de Gruissan et elle s’étend sur 1,2km.</p>
-       <p class="mt-4">Pour vous rendre sur cette plage depuis le port de Gruissan, il suffit de prendre l’avenue des plages, puis de passer le pont qui passe au dessus de l’étang de Mateille.</p>
-       <p class="mt-4">Cette plage dispose d’un grand parking situé juste après le petit pont qui passe au dessus de l’Étang de Mateille. </p>
-   </div>
-   <div class="mt-3 flex justify-center">
-    <img class="rounded-[40px] w-[312px] h-[189px] object-cover" src="../../public/images/festi_img2.webp" alt="">
-   </div>
-   <h2 class="mt-10 ml-7 font-lato text-sm text-Extended/true-gray/50 font-bold">Deux grandes scènes</h2>
-   <div class="mt-3 flex justify-center">
-    <img class="relative rounded-[40px] w-[312px] h-[189px] opacity-80 object-cover" src="../../public/images/festi_pulcoplaya.webp" alt="">
-    <p class= "absolute my-16 text-4xl font-concert-one text-Extended/true-gray/50">Pulco Playa</p>
-   </div>
-   <div class="mt-3 flex justify-center">
-    <img class="relative rounded-[40px] w-[312px] h-[189px] opacity-80 object-cover" src="../../public/images/festi_acaplaya.webp" alt="">
-     <p class= "absolute my-16 text-4xl font-concert-one text-Extended/true-gray/50">Aca Playa</p>
-   </div>
-   <h2 class="mt-10 ml-7 font-lato text-sm text-Extended/true-gray/50 font-bold">Retour en images</h2>
-   <h4 class="mt-5 ml-7 font-concert-one text-sm text-Extended/amber/300 font-bold">Summer</h4>
-    <div class=" mt-2 w-full flex gap-0 snap-x overflow-x-auto">
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_sum1.webp" alt="">  
+    <!-- Format Desktop -->
+    <div class="hidden flex-col align-middle md:flex">
+      <div class="mt-28 flex flex-row gap-16">
+        <div class="ml-12">
+          <img
+            class="max-h-[189px] w-full rounded-[40px] opacity-80 lg:max-h-[345px]"
+            src="../../public/images/festi_img1.webp"
+            alt="Personnes qi font la fête"
+          />
         </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_sum2.webp" alt="">  
+        <div class="mr-16 w-[670px] font-lato text-sm text-Extended/true-gray/50 lg:text-xl">
+          <p>Acapul’co est un Festival situé dans le département de l’Aude est plus précisement dans le petit village de Gruissan.</p>
+          <p class="mt-4">
+            C’est un festival de musique principalement de l’éléctro avec des dj qui viennent du monde entier spécialement pour vous !
+          </p>
+          <p class="mt-4">
+            Le festival est composé de deux grandes scènes, de plusieurs espaces pour pouvoir s’hydrater et se ressoucer. Ainsi que des
+            espaces pour vous amusez et remportez des goodies. Et à la fin de la soirée nous vous préparons d’autres surprises qui sont à
+            venir découvrir...
+          </p>
         </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_sum3.webp" alt="">  
+      </div>
+      <h2 class="mt-28 ml-7 font-lato text-lg font-bold text-Extended/true-gray/50 lg:text-2xl">La plage de Mateille</h2>
+      <div class="mt-10 flex flex-row gap-16">
+        <div class="ml-16 w-[670px] font-lato text-sm text-Extended/true-gray/50 lg:text-xl">
+          <p>
+            La plage de Mateille est situé entre l’Étang de Mateille et l’entrée du port. C’est l’une des plages les plus sauvages de
+            Gruissan et elle s’étend sur 1,2km.
+          </p>
+          <p class="mt-4">
+            Pour vous rendre sur cette plage depuis le port de Gruissan, il suffit de prendre l’avenue des plages, puis de passer le pont
+            qui passe au dessus de l’étang de Mateille.
+          </p>
+          <p class="mt-4">
+            Cette plage dispose d’un grand parking situé juste après le petit pont qui passe au dessus de l’Étang de Mateille.
+          </p>
         </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_sum4.webp" alt="">  
+        <div class="mt-3">
+          <div class="mr-12">
+            <img
+              class="max-h-[189px] w-full rounded-[40px] opacity-80 lg:max-h-[345px]"
+              src="../../public/images/festi_img2.webp"
+              alt="Image de la plage de Mateille"
+            />
+          </div>
         </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_sum5.webp" alt="">  
+      </div>
+      <div class="mx-7 mt-10 flex justify-center">
+        <img src="../../public/images/festi_carte.png" alt="Carte de la France" />
+      </div>
+      <h2 class="mt-28 ml-7 font-lato text-lg font-bold text-Extended/true-gray/50 lg:text-2xl">Deux grandes scènes</h2>
+      <div class="mt-10 flex flex-row justify-center gap-20">
+        <div class="mt-3 flex justify-center">
+          <img
+            class="relative max-h-[189px] w-full rounded-[40px] object-cover opacity-80 lg:max-h-[345px]"
+            src="../../public/images/festi_pulcoplaya.webp"
+            alt=""
+          />
+          <p class="absolute my-16 font-concert-one text-4xl text-Extended/true-gray/50 lg:my-24 lg:text-5xl">Pulco Playa</p>
         </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_sum6.webp" alt="">  
+        <div class="mt-3 flex justify-center">
+          <img
+            class="relative max-h-[189px] w-full rounded-[40px] object-cover opacity-80 lg:max-h-[345px]"
+            src="../../public/images/festi_acaplaya.webp"
+            alt=""
+          />
+          <p class="absolute my-16 font-concert-one text-4xl text-Extended/true-gray/50 lg:my-24 lg:text-5xl">Aca Playa</p>
         </div>
+      </div>
+      <h2 class="mt-28 ml-7 font-lato text-lg font-bold text-Extended/true-gray/50 lg:text-2xl">Retour en images</h2>
+      <div class="bg-Extended/amber/300">
+        <h4 class="mt-5 ml-7 font-concert-one text-sm font-bold text-Extended/true-gray/50">Summer</h4>
+      </div>
     </div>
-    <h4 class="mt-5 ml-7 font-concert-one text-sm text-Extended/amber/300 font-bold">Night</h4>
-    <div class=" mt-2 w-full flex gap-0 snap-x overflow-x-auto">
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_night1.webp" alt="">  
-        </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_night2.webp" alt="">  
-        </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_night3.webp" alt="">  
-        </div>
+
+    <img class="w-full object-cover" src="../../public/images/festi_sum1.webp" alt="" />
+
+    <img class="w-full object-cover" src="../../public/images/festi_sum2.webp" alt="" />
+
+    <img class="w-full object-cover" src="../../public/images/festi_sum3.webp" alt="" />
+
+    <img class="w-full object-cover" src="../../public/images/festi_sum4.webp" alt="" />
+
+    <img class="w-full object-cover" src="../../public/images/festi_sum5.webp" alt="" />
+
+    <img class="w-full object-cover" src="../../public/images/festi_sum6.webp" alt="" />
+
+    <h4 class="mt-5 ml-7 font-concert-one text-sm font-bold text-Extended/amber/300">Night</h4>
+    <div class="mt-2 flex w-full snap-x gap-0 overflow-x-auto">
+      <div class="shrink-0 snap-center first:pl-5 last:pr-5">
+        <img class="w-full object-cover" src="../../public/images/festi_night1.webp" alt="" />
+      </div>
+      <div class="shrink-0 snap-center first:pl-5 last:pr-5">
+        <img class="w-full object-cover" src="../../public/images/festi_night2.webp" alt="" />
+      </div>
+      <div class="shrink-0 snap-center first:pl-5 last:pr-5">
+        <img class="w-full object-cover" src="../../public/images/festi_night3.webp" alt="" />
+      </div>
     </div>
-    <h4 class="mt-5 ml-7 font-concert-one text-sm text-Extended/amber/300 font-bold">Fireworks</h4>
-    <div class=" mt-2 pb-5 w-full flex gap-0 snap-x overflow-x-auto">
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_fire1.webp" alt="">  
-        </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_fire2.webp" alt="">  
-        </div>
-        <div class="snap-center shrink-0 first:pl-5 last:pr-5">
-              <img class="w-full object-cover" src="../../public/images/festi_fire3.webp" alt="">  
-        </div>
+    <h4 class="mt-5 ml-7 font-concert-one text-sm font-bold text-Extended/amber/300">Fireworks</h4>
+    <div class="mt-2 flex w-full snap-x gap-0 overflow-x-auto pb-5">
+      <div class="shrink-0 snap-center first:pl-5 last:pr-5">
+        <img class="w-full object-cover" src="../../public/images/festi_fire1.webp" alt="" />
+      </div>
+      <div class="shrink-0 snap-center first:pl-5 last:pr-5">
+        <img class="w-full object-cover" src="../../public/images/festi_fire2.webp" alt="" />
+      </div>
+      <div class="shrink-0 snap-center first:pl-5 last:pr-5">
+        <img class="w-full object-cover" src="../../public/images/festi_fire3.webp" alt="" />
+      </div>
     </div>
-    </main>
-    <footer>
-        <Footer/>
-    </footer>
+  </main>
+  <footer>
+    <Footer />
+  </footer>
 </template>
 
 <script>
-import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 
 export default {
-    components: {Header, Footer}
-}
+  components: { Header, Footer },
+};
 </script>
