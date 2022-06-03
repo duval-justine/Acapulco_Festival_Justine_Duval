911326f6cbd2423bf9eadda3df6553cd9373b00f
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-06-03 16:30:59 +0200

    Suppression de artistes admin et modif lien crea

--- src/pages/CreaArtistes.vue
+++ src/pages/CreaArtistes.vue
@@ -2,21 +2,18 @@
   <header>
     <Header />
   </header>
-  <h1 class="mt-14 text-center font-concert-one text-3xl font-bold text-Extended/true-gray/50">Création Artiste</h1>
+  <h1 class="mt-14 text-center font-concert-one text-3xl font-bold text-Extended/true-gray/50">Création d' un artiste</h1>
   <div class="relative mt-12 flex flex-col gap-20 px-5">
-    <div class="flex items-end justify-between">
-      <h1 class="font-lato text-4xl underline">Ajouter Artiste</h1>
-    </div>
     <form @submit.prevent="createArtiste">
       <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
         <div class="grid place-items-center">
           <img :src="imageData" class="w-1/2" />
         </div>
         <div class="grid grid-cols-1 gap-14">
-          <div class="flex h-10 overflow-hidden rounded-sm bg-zinc-400 font-lato text-black">
-            <div class="mt-14 text-center font-concert-one text-3xl font-bold text-Extended/true-gray/50">Nom</div>
+          <div class="flex h-10 overflow-hidden rounded-sm bg-Extended/true-gray/50 font-lato text-sm text-Extended/true-gray/900">
+            <div class="mt-14 bg-Extended/amber/500 text-center font-lato text-3xl font-bold text-Extended/true-gray/900">Nom</div>
             <input
-              class="mt-5 bg-Extended/true-gray/50 font-lato text-lg"
+              class="mt-5 bg-Extended/true-gray/50 pb-2 font-lato text-lg"
               type="text"
               placeholder="Nom de l'artiste"
               v-model="Artistes.nom"
@@ -24,10 +21,10 @@
             />
           </div>
 
-          <div class="flex h-10 overflow-hidden rounded-sm bg-zinc-400 font-lato text-black">
-            <div class="mt-14 text-center font-concert-one text-3xl font-bold text-Extended/true-gray/50">Date</div>
+          <div class="flex h-10 overflow-hidden rounded-sm bg-Extended/true-gray/50 font-lato text-sm text-Extended/true-gray/900">
+            <div class="mt-14 bg-Extended/amber/500 text-center font-lato text-3xl font-bold text-Extended/true-gray/900">Date</div>
             <input
-              class="mt-5 bg-Extended/true-gray/50 font-lato text-lg"
+              class="mt-5 bg-Extended/true-gray/50 pb-2 font-lato text-lg"
               type="text"
               placeholder="Date du concert"
               v-model="Artistes.date"
@@ -35,10 +32,10 @@
             />
           </div>
 
-          <div class="flex h-10 overflow-hidden rounded-sm bg-zinc-400 font-lato text-black">
-            <div class="mt-14 text-center font-concert-one text-3xl font-bold text-Extended/true-gray/50">Lien</div>
+          <div class="flex h-10 overflow-hidden rounded-sm bg-Extended/true-gray/50 font-lato text-sm text-Extended/true-gray/900">
+            <div class="mt-14 bg-Extended/amber/500 text-center font-lato text-3xl font-bold text-Extended/true-gray/900">Lien</div>
             <input
-              class="mt-5 bg-Extended/true-gray/50 font-lato text-lg"
+              class="mt-5 bg-Extended/true-gray/50 pb-2 font-lato text-lg"
               type="text"
               placeholder="Lien de la page"
               v-model="Artistes.lien"
@@ -46,11 +43,13 @@
             />
           </div>
 
-          <div class="flex h-10 overflow-hidden rounded-sm bg-zinc-400 text-black">
-            <div class="bg-true-gray-25 flex items-center justify-center border-[1px] px-5 font-lato">Photo</div>
+          <div class="flex h-10 overflow-hidden rounded-sm bg-Extended/true-gray/50 font-lato text-sm text-Extended/true-gray/900">
+            <div class="mt-14 bg-Extended/amber/500 text-center font-lato text-3xl font-bold text-Extended/true-gray/900">Photo</div>
             <div class="relative w-full">
-              <input type="file" class="relative w-full" ref="file" id="file" @change="previewImage" />
-              <label class="absolute left-0 top-0 bottom-0 flex w-full items-center justify-center bg-gray-300 p-3 font-lato" for="file"
+              <input type="file" class="relative w-full pb-2" ref="file" id="file" @change="previewImage" />
+              <label
+                class="absolute left-0 top-0 bottom-0 flex w-full items-center justify-center bg-Extended/true-gray/50 p-3 font-lato"
+                for="file"
                 >Sélectionner l'image</label
               >
             </div>
@@ -58,17 +57,23 @@
         </div>
       </div>
       <div class="mt-16 grid w-full grid-cols-2 place-items-center">
-        <button class="w-fit rounded-xl bg-red-700 px-16 py-4 font-lato text-white" type="submit">Ajouter</button>
-        <button class="w-fit rounded-xl bg-red-700 px-16 py-4 font-lato text-white" type="button">
-          <RouterLink to="/gestion">Annuler</RouterLink>
+        <button class="w-fit rounded-xl bg-Extended/amber/500 px-16 py-4 font-lato text-white hover:bg-Extended/amber/400" type="button">
+          <RouterLink to="/gestionartistes">Annuler</RouterLink>
+        </button>
+        <button class="w-fit rounded-xl bg-Extended/amber/500 px-16 py-4 font-lato text-white hover:bg-Extended/amber/400" type="submit">
+          Ajouter
         </button>
       </div>
     </form>
   </div>
+  <footer class="pt-20">
+    <Footer />
+  </footer>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 import {
   getFirestore,
   collection,
@@ -82,6 +87,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
 import { getStorage, ref, getDownloadURL, uploadString } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
 export default {
+  components: { Header, Footer },
   data() {
     return {
       imageData: null,
@@ -117,8 +123,8 @@ export default {
         const db = getFirestore();
         const docRef = addDoc(collection(db, "Artistes"), this.Artistes);
       });
-      this.$router.push("/gestion");
-      this.$router.push("../Artistes");
+      this.$router.push("/gestionartiste");
+      this.$router.push("/artistes");
     },
   },
 };
