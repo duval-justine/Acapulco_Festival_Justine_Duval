f11e7ba7f2ddc5008655a6fa13139b9551dcad0e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-06-03 10:46:20 +0200

    Modif taille carrousel page accueil

--- src/components/CarrouselAcc.vue
+++ src/components/CarrouselAcc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-Extended/true-gray/900">
-    <img class="w-full object-cover" :src="image" alt="" />
+    <img class="w-full object-cover lg:max-h-[550px]" :src="image" alt="" />
     <p class="py-2 text-center font-lato text-sm text-Extended/true-gray/50 drop-shadow-md md:text-xl">{{ texte }}</p>
   </div>
 </template>
