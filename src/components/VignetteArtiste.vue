bf868dd4a07b0a794bf50a88e0d54df15f39d46d
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-06-03 14:32:36 +0200

    Vignette Artiste responsive

--- src/components/VignetteArtiste.vue
+++ src/components/VignetteArtiste.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="relative mx-11 flex justify-center">
-    <div class="absolute mt-[90px] h-[190px] w-[287px] rounded-16xl bg-Extended/amber/300"></div>
-    <div class="absolute ml-8 mt-[105px] flex flex-col gap-4">
-      <a href="https://www.instagram.com/martingarrix/?hl=fr"><Insta class="h-5 w-5" /></a>
-      <a href="https://twitter.com/MartinGarrix"><Twitter class="h-5 w-5" /></a>
-      <a href="https://www.facebook.com/martin.garrix"><Facebook class="h-5 w-5" /></a>
-      <a href="https://www.youtube.com/channel/UC5H_KXkPbEsGs0tFt8R35mA"><YouTube class="h-5 w-5" /></a>
-      <a href="https://martingarrix.com/"><LinkIcon class="h-5 w-5 text-Extended/true-gray/50" /></a>
+  <div class="mx-11 flex justify-center">
+    <div class="mt-[90px] h-[190px] w-[287px] rounded-16xl bg-Extended/amber/300 lg:h-[379.2px] lg:w-[572.79px]">
+      <div class="ml-8 mt-[15px] flex flex-col gap-4 lg:mt-14">
+        <a href="https://www.instagram.com/martingarrix/?hl=fr"><Insta class="h-5 w-5 lg:h-10 lg:w-10" /></a>
+        <a href="https://twitter.com/MartinGarrix"><Twitter class="h-5 w-5 lg:h-10 lg:w-10" /></a>
+        <a href="https://www.facebook.com/martin.garrix"><Facebook class="h-5 w-5 lg:h-10 lg:w-10" /></a>
+        <a href="https://www.youtube.com/channel/UC5H_KXkPbEsGs0tFt8R35mA"><YouTube class="h-5 w-5 lg:h-10 lg:w-10" /></a>
+        <a href="https://martingarrix.com/"><LinkIcon class="h-5 w-5 text-Extended/true-gray/50 lg:h-10 lg:w-10" /></a>
+      </div>
+      <img
+        class="absolute -mt-[225px] ml-5 h-[237px] w-[267px] lg:ml-[40px] lg:-mt-[413px] lg:h-[473px] lg:w-[532.87px]"
+        src="../../public/images/martin_garrix.png"
+        alt=""
+      />
     </div>
-    <img class="absolute ml-4 mt-10" src="../../public/images/martin_garrix.png" alt="" />
   </div>
 </template>
 
